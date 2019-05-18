5ca1706dc84705c3d966075b169009e32700a06f
xxie54 <47068487+xxie54@users.noreply.github.com>
2019-05-18 16:03:03 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class void Main implements Comparable<int[]>{
+public static void main(String[] args) {
+
+@Override
+int[] nums={6,-1,-2,-3,0,1,2,3,4};
+Arrays.sort(nums);
+System.out.println("Maximum = " + nums[nums.length-1]);
+
+  }
+}
